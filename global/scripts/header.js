9e23b97de9febca042f4c10fc0421d48bc587fbf
HaileabT <Haileabtesfaye11@gmail.com>
2024-01-19 11:30:48 +0300

    finished up the header

--- global/scripts/header.js
+++ global/scripts/header.js
@@ -1,11 +1,21 @@
+const menuBtn = document.querySelector('.menu-btn')
+const primaryNav = document.getElementById('primary-nav')
+const fixedHeader = document.querySelector('.fixed-header')
+const menuClickHandler = (e) => {
+    console.log(e.currentTarget)
+    primaryNav.classList.toggle('show')
+    fixedHeader.classList.toggle('show')
+}
+menuBtn.addEventListener('click', menuClickHandler, true)
 function fixedHeaderController() {
 
     if (window.matchMedia('(hover:hover)').matches){
-        const fixedHeader = document.querySelector('.fixed-header')
         window.addEventListener('scroll', controlHeader, { passive: true })
 
         function controlHeader(e) {
             if (window.scrollY < 100){
+                primaryNav.classList.remove('show')
+                fixedHeader.classList.remove('show')
                 fixedHeader.style.top = '1em'
                 return
             }
@@ -17,5 +27,4 @@ function fixedHeaderController() {
     }
 
 }
-
 fixedHeaderController()
